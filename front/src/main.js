2751f3b4eb3872e68a934f5b212503f041bf86d2
Sam ASUS <sam.montaudon@gmail.com>
2021-11-26 15:24:31 +0100

    Ajout du personnage bas de page

--- front/src/main.js
+++ front/src/main.js
@@ -3,15 +3,18 @@ import Vue from 'vue'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
 import '/src/assets/scss/_resets.scss'
-import '/src/assets/scss/custom.scss'
+import '@/assets/scss/custom.scss'
 
-
+import Minion from './components/Minion.vue';
 
 // d'abord bootstrap puis bootstrap-vue
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.component('Minion', Minion);
+
 Vue.config.productionTip = false
+
 Vue.use(BootstrapVue)
 
 new Vue({
