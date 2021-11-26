433b4bf170b8d303c3130d375c723447fd373828
Sam ASUS <sam.montaudon@gmail.com>
2021-11-26 15:25:22 +0100

    Categories

--- front/src/views/Categories.vue
+++ front/src/views/Categories.vue
@@ -1,74 +1,121 @@
 <template>
   <div>
     <b-row
-      class="justify-content-center align-content-center m-0 categories-list"
+      class="justify-content-center align-content-center m-0 categories-list p-5 "
     >
-      <img src="/src/assets/img/sport.png" alt="" />
       <div
         id="v-cats"
         class="
-          col-3
+          col-6
+          col-md-3
           category-item
           d-flex
           flex-column
           justify-content-center
           p-0
         "
-
         v-for="categorie in categories"
         :key="categorie.id"
       >
-        <router-link :to="{ name: 'id_Categorie', params: { id: categorie.id }}">
+        <router-link
+          :to="{ name: 'id_Categorie', params: { id: categorie.id } }"
+        >
           <div>
-            <img :src="tabIcon[categorie.id - 1]" width="50px" alt="" />
+            <img
+              class="cat_img"
+              :src="tabIcon[categorie.id - 1]"
+              width="200px"
+              alt=""
+            />
+            <p class="cat_name">
+              {{ categorie.name }}
+            </p>
           </div>
-          <p>
-            {{ categorie.name }}
-          </p>
         </router-link>
       </div>
-    </b-row>    
+    </b-row>
+    <b-row class="w-75 justify-content-end ">
+      <Minion title="Catégories"/>
+    </b-row>
   </div>
 </template>
 
 <script>
-import sport from "../assets/img/sport.png";
-import musique from "../assets/img/musique.png";
-import cinema from "../assets/img/cinema.png"
-import informatique from "../assets/img/sport.png"
-import animaux from "../assets/img/sport.png"
-import culture from "../assets/img/sport.png"
-  import axios from "axios";
+import sport from '../assets/img/sport.png'
+import musique from '../assets/img/musique.png'
+import cinema from '../assets/img/cinema.png'
+import informatique from '../assets/img/informatique.png'
+import animaux from '../assets/img/animaux.png'
+import culture from '../assets/img/culture.png'
+
+import axios from 'axios'
 
 export default {
-  name: "Categorie",
-    data() {
+  name: 'Categorie',
+
+  data () {
     return {
       tabIcon: [informatique, musique, animaux, sport, culture, cinema],
-      categories: null,
-    };
+      categories: null
+    }
   },
+
   components: {},
   methods: {},
-  mounted() {
+  mounted () {
     axios
-      .get("http://localhost:8080/categories")
-      .then((response) => (this.categories = response.data));
-  },
-};
+      .get('http://localhost:5000/categories')
+      .then(response => (this.categories = response.data))
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-@import "/src/assets/scss/custom.scss";
+@import '/src/assets/scss/custom.scss';
 
 .categories-list {
   gap: 1rem;
 }
 
+.cat_name {
+  display: inline-block;
+  position: absolute;
+  left: 0;
+  top: 43%;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 1.5rem;
+  transition: all 0.2s linear;
+  color: transparent;
+}
+
+.cat_img {
+  transition: all 0.25s ease-in-out;
+}
+
 .category-item {
   flex-basis: 33.33333%;
-  border-radius: 12px;
-  border: 2px solid black;
-  background: $white;
+  position: relative;
+  top: 0;
+  z-index: 3;
+
+  &:hover {
+    .cat_img {
+      filter: blur(0.1rem) brightness(0.69);
+      transform: rotate(10deg);
+    }
+
+    .cat_name {
+      color: $white;
+      font-weight: 800;
+    }
+  }
+
+  &:active {
+    .cat_img {
+      transform: scale(0.9);
+    }
+  }
 }
 </style>
