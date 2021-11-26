26f59e4cdf9ef2087db04418cdaea54a0509953d
JulieMlr <julie.miler.jm@gmail.com>
2021-11-26 14:35:45 +0100

    [UPD] liste des quiz

---
+++ front/src/views/Liste_quiz.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Liste des quiz</h1>
+    <div v-for="quiz in quiz" :key="quiz.id">
+      {{ quiz.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "liste_quiz",
+  data() {
+    return {
+      idCategorie: null,
+      quizs: null,
+    };
+  },
+  mounted() {
+    this.idCategorie = this.$route.params.id;
+    console.log(this.idCategorie);
+    axios
+      .get("http://localhost:8080/quiz", { params: { id: this.idCategorie } })
+      .then((response) => (this.quizs = response.data));
+    console.log(this.quizs);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
