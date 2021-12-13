9fc09aee387e36e62d7ce2b30afe961f39ea776f
SCHOEBELA <Pschoebela@gmail.com>
2021-12-13 15:01:31 +0100

    Fix QuizzRoutes
    Fix Questions

--- Back/routes/question.routes.js
+++ Back/routes/question.routes.js
@@ -1,53 +1,14 @@
-const path = require('path')
-const HOMEDIR = path.join(__dirname, '..');
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const QuizzController = require('../controllers/category');
 
-const QuizQuestionsService = require(path.join(HOMEDIR, 'services', 'quizQuestions-service'));
-const quizQuestions = require('../models/question')
-//route GetALL Categories
+// views
+router.get('/', QuizzController.findCategories);
+router.get('/:id', QuizzController.findCategory);
 
-router.get("/", async(req,res)=>{
-    const allQuizQuestions = await QuizQuestionsService.findAllQuizQuestions(req.query);
-    res.json(allQuizQuestions);
+// actions
+router.post('/create', QuizzController.create);
+router.put('/:id/update', QuizzController.update);
+router.delete('/:id/delete', QuizzController.delete);
 
-})
-
-//route POSTALL san id 
-
-router.post("/", async(req,res)=>{
-    const tryToSave = await QuizQuestionsService.saveOne(req.body);
-    res.json(tryToSave);
-})
-
-
-//Get Categorie  avec Id
-
-router.get('/:id', async(req, res)=>{
-    console.log(req.params)
-    const {id} = req.params;
-    const quizQuestions = await QuizQuestionsService.findOneQuizQuestion(id);
-    res.json(quizQuestions)
-})
-
-
-// PUT avec id 
-
-router.put("/:id", async(req, res)=>{
-    const {id}=req.params
-
-    const {name}=req.body
-    quizQuestions.findOneAndUpdate({id},{name})
-    .then(quizQuestions=>res.send(quizQuestions))
-    .catch(err=>console.log(err))
-})
-
-//Delete avec id 
-
-router.delete("/:id", async(req, res)=>{
-    const {id}=req.params
-    const quizQuestions = await QuizQuestionsService.deleteOne(id);
-    res.json(quizQuestions)
-})
-
-module.exports = router
+module.exports = router;
