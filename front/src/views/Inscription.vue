31004f84ee9afc0912b017586a252251eabfd114
JulieMlr <julie.miler.jm@gmail.com>
2021-12-16 09:25:56 +0100

    [ADD] formulaire inscription

---
+++ front/src/views/Inscription.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form>
+      <div class="wrapper">
+        <input type="text" id="name" name="user_name" placeholder="Nom" />
+        <input
+          type="text"
+          id="prenom"
+          name="user_prenom"
+          placeholder="Prénom"
+        />
+        <input type="email" id="mail" name="user_mail" placeholder="Mail" />
+        <input
+          type="pasword"
+          id="pasword"
+          name="user_pasword"
+          placeholder="Mot de passe"
+        />
+      </div>
+      <button type="submit">Inscription</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inscription",
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin: 30px auto;
+}
+input {
+  margin-bottom: 20px;
+}
+</style>
