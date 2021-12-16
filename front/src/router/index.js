6c1cf3a99874b4bf3fdec8df94267ba7ccf4e5ee
JulieMlr <julie.miler.jm@gmail.com>
2021-12-16 09:25:56 +0100

    [ADD] formulaire inscription

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -48,6 +48,15 @@ const routes = [
   }
   ,
   {
+    path: '/inscription',
+    name: 'inscription',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Inscription.vue')
+  }
+  ,
+  {
     path: '/Signup',
     name: 'Signup',
     // route level code-splitting
